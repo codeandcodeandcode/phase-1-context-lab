bcb1121cafe04b782c6f1f79fa84e47e79ee7253
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-26 22:41:18 +0300

    Added wagesEarnedOnDate function

--- index.js
+++ index.js
@@ -36,4 +36,8 @@ const allWagesFor = function () {
 
     return payable
 }
+function wagesEarnedOnDate(obj, dt) {
+    let c = Math.floor(hoursWorkedOnDate(obj, dt) * obj.payPerHour);
+    return c;
+}
 
